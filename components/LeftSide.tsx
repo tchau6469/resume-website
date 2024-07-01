7a00607a5e33968abd5d2499256a4033236c832b
Tony <tchau6469@gmail.com>
2024-07-01 18:23:15 -0500

    changed navbar to be all tailwind

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -5,29 +5,29 @@ import ResumeButton from "./ResumeButton";
 const items = ["about", "experience", "projects"];
 
 export default function LeftSide({ className }: { className: string }) {
-  return (    
-    <div className={"relative "+ className}>
+  return (
+    <div className={"relative " + className}>
       <header>
         <h1 className="mb-2 text-5xl font-bold tracking-tight text-slate-200">
-          <Link href="/">this is branch</Link>
+          <Link href="/">Tony Chau</Link>
         </h1>
         <h2 className="mb-2 text-xl text-slate-200">Software Engineer</h2>
-        <ResumeButton ></ResumeButton>
+        <ResumeButton></ResumeButton>
 
-        <p className="max-w-xs text-slate-400 text-">
-          If you
-          need a programmer who’s great at solving problems (current problem:
-          unemployment), then I’m your guy!
+        <p className="text- max-w-xs text-slate-400">
+          If you need a programmer who’s great at solving problems (current
+          problem: unemployment), then I’m your guy!
         </p>
 
         <nav className="my-14 hidden lg:inline-block">
           <ul>
+
           
             {items.map((item) => (
               <li key={item} className="my-4">
-                <Link className="nav-text-with-line" href={`#${item}`}>
-                  <div className="nav-line"></div>
-                  <div className="nav-text text-xs font-bold uppercase tracking-wide">
+                <Link className="group flex items-center" href={`#${item}`}>
+                  <div className="mr-4 h-[1px] w-8 bg-slate-500 transition-all duration-300 group-hover:w-16 group-hover:bg-slate-200 group-focus:w-16 group-focus:bg-slate-200"></div>
+                  <div className="transition-color text-xs font-bold uppercase tracking-wide text-slate-500 duration-300 group-hover:text-slate-200 group-focus:text-slate-200">
                     {item}
                   </div>
                 </Link>
@@ -35,14 +35,9 @@ export default function LeftSide({ className }: { className: string }) {
             ))}
           </ul>
         </nav>
-        
-        
-
-        
       </header>
 
       <BottomLinks></BottomLinks>
-  </div>
-    
+    </div>
   );
 }
